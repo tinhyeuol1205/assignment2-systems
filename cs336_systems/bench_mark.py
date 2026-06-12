41ca92bbd09858eabb095dfcbe7f0cdb77a15ee5
Doan Van Binh <nhathotinhtuyetvong.241201@gmail.com>
2026-06-12 15:19:33 +0700

    perf: add NVTX instrumentation to benchmark loops and reduce iteration count

--- cs336_systems/bench_mark.py
+++ cs336_systems/bench_mark.py
@@ -3,6 +3,7 @@ import time
 
 from cs336_basics_new.optim import AdamW
 from cs336_basics_new.nn import cross_entropy, TransformerLM, ModelConfig
+import torch.cuda.nvtx as nvtx
 
 model_config = ModelConfig(
     vocab_size=10000,
@@ -30,22 +31,28 @@ model.train()
 forward_time = []
 backward_time = []
 optimizer_time = []
-for i in range(20):
+for i in range(3):
     start = time.time()
+    nvtx.range_push("Forward")
     output = model(sample_input)
     torch.cuda.synchronize() # Wait for GPU to finish
+    nvtx.range_pop()
     forward_stone = time.time()
     forward_time.append(forward_stone - start)
     start_backward = time.time()
+    nvtx.range_push("Backward")
     simulate_loss = cross_entropy(output, sample_input)
     optimizer.zero_grad()
     simulate_loss.backward()
     torch.cuda.synchronize() # Wait for GPU to finish
+    nvtx.range_pop()
     backward_stone = time.time()
-    backward_time.append(backward_stone - start_backward)
+    backward_time.append(backward_stone - start_backward) 
     optimizer_start = time.time()
+    nvtx.range_push("Optimization")
     optimizer.step()
     torch.cuda.synchronize() # Wait for GPU to finish
+    nvtx.range_pop()
     optimizer_stone = time.time()
     optimizer_time.append(optimizer_stone - optimizer_start)
 print(f"Average forward time: {sum(forward_time)/len(forward_time):.4f} seconds")
